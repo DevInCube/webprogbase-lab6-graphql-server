76cb33216b18552bff968143c3a52072d386b0b5
phadyniak <public.hadyniak@gmail.com>
2021-03-30 01:41:59 +0300

    me auth

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const fs = require('fs');
-const { PubSub, ApolloServer } = require('apollo-server');
+const fs = require("fs");
+const { PubSub, ApolloServer } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+const config = require('./config');
 
 const pubsub = new PubSub();
 
 const resolvers = {
-    Query: require('./resolvers/Query'),
-    Mutation: require('./resolvers/Mutation'),
-    Subscription: require('./resolvers/Subscription'),
-  };
+  Query: require("./resolvers/Query"),
+  Mutation: require("./resolvers/Mutation"),
+  Subscription: require("./resolvers/Subscription"),
+};
 
 const server = new ApolloServer({
-    cors: true,
-    typeDefs: fs.readFileSync('./schema.gql').toString(),
-    resolvers,
-    context: (req) => ({
+  cors: true,
+  typeDefs: fs.readFileSync("./schema.gql").toString(),
+  resolvers,
+  context: (req) => {
+    return {
         pubsub,
-    }),
+        req: req.req,
+    };
+  },
 });
 
-server
-    .listen()
-    .then(({ url, subscriptionsUrl }) => {
-        console.log(`🚀 Server ready at ${url}`);
-        console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
-    });
+server.listen().then(({ url, subscriptionsUrl }) => {
+  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
+});
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,17 +1,34 @@
 const jwt = require('jsonwebtoken');
 const config = require('./../config');
 
+async function getUser(req) {
+    const token = (req.headers.authorization || "").substring("Bearer ".length);
+    if (!token) {
+        throw new Error("Not authenticated");
+    }
+
+    const decoded = jwt.verify(token, config.jwtSecret);
+    return decoded;
+}
+
 module.exports = {
     me: (parent, args, context) => {
-        console.log(context);
-        if (!context.user) {
+        const {req} = context;
+        const user = getUser(req);
+        
+        if (!user) {
             throw new Error("Not authorized");
         }
 
-        return context.user;
+        return user;
     },
     login: (parent, {login, password}, context) => {
-        const token = jwt.sign({login}, config.jwtSecret);
+        const fakeId = Math.random() * 100000 | 0;
+        const fakeUser = {
+            id: fakeId,
+            login,
+        };
+        const token = jwt.sign(fakeUser, config.jwtSecret);
         return token;
     }
 }
