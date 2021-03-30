a600b462d7ecb40b4683531531b7a71dd4366192
Ruslan Hadyniak <rushad@auditdata.com>
2021-03-30 13:14:46 +0300

    renamed usernameExists method

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -11,7 +11,7 @@ module.exports = {
 
         return getUser();
     },
-    async userNameExists(_, {username}, {database}) {
+    async usernameExists(_, {username}, {database}) {
         const existingUser = await database.getUserByUsername(username);
         return !!existingUser;
     },
