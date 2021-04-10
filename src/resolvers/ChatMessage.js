ed517a40480644eba5ff86640893d42dce264bda
phadyniak <public.hadyniak@gmail.com>
2021-04-10 19:09:07 +0300

    add message.author resolver

--- src/resolvers/ChatMessage.js
+++ src/resolvers/ChatMessage.js
@@ -2,6 +2,9 @@ module.exports = {
     async timestamp(message, {}, {}) {
         return new Date(message.timestamp).toISOString();
     },
+    async author(message, {}, {}) {
+        return database.getUserById(message.author);
+    },
     async room(message, {}, {database}) {
         return database.getRoom(message.room);
     }
